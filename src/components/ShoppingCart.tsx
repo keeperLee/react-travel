4e479266a0ee0ef2432caec4c7a4aee5aaf8d244
lijian <ljhhh@qq.com>
2022-03-28 00:54:54 +0800

    React的事件处理

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './ShoppingCart.module.css'
+import {FiShoppingCart} from "react-icons/fi";
+
 interface Props {
 
 }
@@ -9,25 +11,33 @@ interface State {
 }
 
 class ShoppingCart extends React.Component<Props, State> {
-    constructor(props:Props) {
+    constructor(props: Props) {
         super(props);
         this.state = {
             isOpen: false,
         }
     }
 
-    render(){
+    handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        console.log(e.target,'e.target') //target描述的是事件发生的元素
+        console.log(e.currentTarget,'e.currentTarget')//currentTarget描述的是事件处理绑定的元素
+        this.setState({isOpen: !this.state.isOpen})
+    }
+
+    render() {
         return (
             <div className={styles.cartContainer}>
                 <button className={styles.button}
-                onClick={()=>{
-                    this.setState({isOpen:!this.state.isOpen})
-                }}
-                >购物车 2 （件）</button>
+                        onClick={
+                            this.handleClick
+                        }
+                >   <FiShoppingCart/>
+                    <span>购物车 2 （件）</span>
+                </button>
                 <div className={styles.cartDropDown}
-                style={{
-                    display:this.state.isOpen ? 'block' : 'none'
-                }}
+                     style={{
+                         display: this.state.isOpen ? 'block' : 'none'
+                     }}
                 >
                     <ul>
                         <li>robot1</li>
