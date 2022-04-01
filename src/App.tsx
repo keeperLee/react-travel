aef6d99aab9582c2286a21b23b370e4e08ca5b2a
lijian <ljhhh@qq.com>
2022-04-02 00:07:20 +0800

    使用useState管理组件state

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,14 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styles from "./App.module.css";
 // import robots from './mockdata/robots.json'
 import Robot from './components/Robot'
 import logo from "./assets/images/logo.svg"
 import ShoppingCart from "./components/ShoppingCart";
 
-interface Props {}
 
-interface State {
-    robotGallery: any[];
-    count:number;
-}
+const App :React.FC = ()=>{
+    const [count,setCount] = useState<number>(0)
 
-class App extends React.Component<Props, State>{
-
-    // *生命周期第一阶段：初始化
-    // 初始化组件 state
-    constructor(props) {
-        super(props);
-        this.state = {
-            robotGallery:[],
-            count:0
-        }
-    }
-
-    //在组件创建好DOM元素后、挂载进页面的时候调用
-    componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then(response => response.json())
-            .then(data=>this.setState({robotGallery:data}))
-    }
-
-    // *生命周期第二阶段：更新
-    // 组件更新后调用
-    componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>, snapshot?: any) {
-    }
-
-    // *生命周期第三阶段：销毁
-    componentWillUnmount() {
-    }
-
-    render(){
        return (
             <div className={styles.app}>
                 <div className={styles.appHeader}>
@@ -48,30 +16,19 @@ class App extends React.Component<Props, State>{
                     <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
                 </div>
                 <button onClick={()=>{
-                    this.setState((preState,preProps)=>{
-                        return {count:preState.count + 1}
-                    },()=>{
-                        console.log("count",this.state.count)
-                    })
-
-                    this.setState((preState,preProps)=>{
-                        return {count:preState.count + 1}
-                    },()=>{
-                        console.log("count",this.state.count)
-                    })
+                    setCount(count+1)
                 }}>Click</button>
-                <span>count:{this.state.count}</span>
+                <span>count:{count}</span>
                 <ShoppingCart/>
-                <div className={styles.robotList}>
-                    {
-                        this.state.robotGallery.map((r) => (
-                            <Robot id={r.id} name={r.name} email={r.email}/>
-                        ))
-                    }
-                </div>
+                {/*<div className={styles.robotList}>*/}
+                {/*    {*/}
+                {/*        this.state.robotGallery.map((r) => (*/}
+                {/*            <Robot id={r.id} name={r.name} email={r.email}/>*/}
+                {/*        ))*/}
+                {/*    }*/}
+                {/*</div>*/}
             </div>
         );
-    }
 }
 
 export default App;
