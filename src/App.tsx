422e790f7c89964670c8cbb1c6a9ae668e8a824f
lijian <ljhhh@qq.com>
2022-04-03 22:50:42 +0800

    使用useEffect的方式来产生副作用

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from "./App.module.css";
 // import robots from './mockdata/robots.json'
 import Robot from './components/Robot'
@@ -6,29 +6,41 @@ import logo from "./assets/images/logo.svg"
 import ShoppingCart from "./components/ShoppingCart";
 
 
-const App :React.FC = ()=>{
-    const [count,setCount] = useState<number>(0)
+const App: React.FC = () => {
+    const [count, setCount] = useState<number>(0)
+    const [robotGallery, setRobotGallery] = useState<any>([])
 
-       return (
-            <div className={styles.app}>
-                <div className={styles.appHeader}>
-                    <img src={logo} className={styles.appLogo} alt="logo"/>
-                    <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
-                </div>
-                <button onClick={()=>{
-                    setCount(count+1)
-                }}>Click</button>
-                <span>count:{count}</span>
-                <ShoppingCart/>
-                {/*<div className={styles.robotList}>*/}
-                {/*    {*/}
-                {/*        this.state.robotGallery.map((r) => (*/}
-                {/*            <Robot id={r.id} name={r.name} email={r.email}/>*/}
-                {/*        ))*/}
-                {/*    }*/}
-                {/*</div>*/}
+    useEffect(() => {
+        document.title = `点击${count}次`
+    }, [count])
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(data => setRobotGallery(data))
+    },[])
+
+    return (
+        <div className={styles.app}>
+            <div className={styles.appHeader}>
+                <img src={logo} className={styles.appLogo} alt="logo"/>
+                <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
             </div>
-        );
+            <button onClick={() => {
+                setCount(count + 1)
+            }}>Click
+            </button>
+            <span>count:{count}</span>
+            <ShoppingCart/>
+            <div className={styles.robotList}>
+                {
+                    robotGallery.map((r) => (
+                        <Robot id={r.id} name={r.name} email={r.email}/>
+                    ))
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
