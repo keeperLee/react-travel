510f01f82bd1fc14f6851bfd8ebd37f2c264d6c0
lijian <ljhhh@qq.com>
2022-04-13 00:00:54 +0800

    组件化头部和底部

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.min.css'; // or 'antd/dist/antd.less'
 import './index.css';
 import App from './App';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 ReactDOM.render(
           <App/>,
