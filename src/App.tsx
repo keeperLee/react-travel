e0db780eb4b238bb804572599ff5f74156719857
lijian <ljhhh@qq.com>
2022-04-15 22:30:20 +0800

    添加基础路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from "./App.module.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route ,Switch} from "react-router-dom";
 import { HomePage } from "./pages";
 
 
@@ -8,7 +8,11 @@ function App() {
     return (
         <div className={styles.App}>
             <BrowserRouter>
-                <Route path="/" component={HomePage} />
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/signIn" render={()=><h1>登录页面</h1>} />
+                    <Route render={()=><h1>404 not found</h1>} />
+                </Switch>
             </BrowserRouter>
         </div>
     );
