707841161f74c92af5b9aa2d8f8e1c1b67e4db98
lijian <ljhhh@qq.com>
2022-04-15 23:53:39 +0800

    分别使用withRoute和useRoute来对路由进行操作

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,34 +1,44 @@
 import React from "react"
 import styles from './Header.module.css'
 import logo from '../../assets/logo.svg'
-import {Layout,Typography,Input,Menu,Button,Dropdown} from "antd";
+import {Layout, Typography, Input, Menu, Button, Dropdown} from "antd";
 import {GlobalOutlined} from "@ant-design/icons"
-export const Header :React.FC = ()=>{
+import {useHistory, useLocation, useParams, useRouteMatch} from "react-router-dom";
+
+export const Header: React.FC = () => {
+
+    const history = useHistory()
+    const location = useLocation()
+    const params = useParams()
+    const match = useRouteMatch()
+
     return (
         <div className={styles['app-header']}>
             <div className={styles['top-header']}>
                 <div className={styles.inner}>
                     <Typography.Text>让旅游更幸福</Typography.Text>
-                    <Dropdown.Button  style={{marginLeft:15}}
-                                      overlay={
-                                          <Menu>
-                                              <Menu.Item>中文</Menu.Item>
-                                              <Menu.Item>English</Menu.Item>
-                                          </Menu>
-                                      } icon={<GlobalOutlined/>}
+                    <Dropdown.Button style={{marginLeft: 15}}
+                                     overlay={
+                                         <Menu>
+                                             <Menu.Item>中文</Menu.Item>
+                                             <Menu.Item>English</Menu.Item>
+                                         </Menu>
+                                     } icon={<GlobalOutlined/>}
                     >
                         语言
                     </Dropdown.Button>
                     <Button.Group className={styles['button-group']}>
-                        <Button>注册</Button>
-                        <Button>登录</Button>
+                        <Button onClick={() => history.push('register')}>注册</Button>
+                        <Button onClick={() => history.push('signIn')}>登录</Button>
                     </Button.Group>
                 </div>
 
             </div>
             <Layout.Header className={styles['main-header']}>
-                <img src={logo} alt="logo" className={styles['App-logo']}/>
+                <span onClick={() => history.push('/')}>
+                 <img src={logo} alt="logo" className={styles['App-logo']}/>
                 <Typography.Title level={3} className={styles.title}>React 旅游网</Typography.Title>
+                </span>
                 <Input.Search placeholder='请输入旅游目的、主题、关键字'
                               className={styles['search-input']}/>
 
